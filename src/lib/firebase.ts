3045d69f693255ee2e0b53907652cc1846c2ca5e
mnsaify <mohammadnazifsaify20@gmail.com>
2025-12-22 17:23:36 -0500

    chore: add debug log for Firebase API key in production

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,6 +4,7 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+console.log('FIREBASE API KEY:', process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
